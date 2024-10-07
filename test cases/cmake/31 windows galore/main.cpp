b21685bf6529eb296e206a6dfbd5678d4dd6f9d9
Momtchil Momtchev <momtchil@momtchev.com>
2024-10-07 19:46:28 +0200

    skip the test properly when not on Windows

--- test cases/cmake/31 windows galore/main.cpp
+++ test cases/cmake/31 windows galore/main.cpp
@@ -1,12 +1,8 @@
-#ifdef WIN32
 #include <windows.h>
 #include <shellapi.h>
-#endif
 
 int main() {
-#ifdef WIN32
   char path[MAX_PATH];
   FindExecutableA("cmd.exe", 0, path);
-#endif
   return 0;
 }
